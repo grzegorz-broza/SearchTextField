6d54dbd95fa42b25038ee609cc159a9e3312c6c1
grzegorz.broza <grzegorz.broza@fotka.com>
2022-06-24 13:16:12 +0200

    update

---
+++ Example/build/Pods.build/Debug-iphonesimulator/Pods-SearchTextField_Example.build/Objects-normal/x86_64/Pods_SearchTextField_Example_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/grzegorzbroza/Documents/DEVEL/SearchTextField/Example/build/Pods.build/Debug-iphonesimulator/Pods-SearchTextField_Example.build/DerivedSources/Pods_SearchTextField_Example_vers.c
---
+++ Example/build/Pods.build/Debug-iphonesimulator/SearchTextField.build/Objects-normal/x86_64/SearchTextField_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/grzegorzbroza/Documents/DEVEL/SearchTextField/Example/build/Pods.build/Debug-iphonesimulator/SearchTextField.build/DerivedSources/SearchTextField_vers.c
